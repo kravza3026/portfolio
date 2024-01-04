9817db1424f3f52d83f7ee68ecc32959fedc015d
kravza <kravza3026@gmail.com>
2024-01-04 18:01:31 +0200

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,15 @@ export class AppComponent implements OnInit{
     el.scrollIntoView({ behavior: 'smooth' });
   }
   ngOnInit() {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+
+
     AOS.init({disable: 'mobile'});//AOS - 2
     AOS.refresh()
   }
--- src/app/components/toggle-mode/toggle-mode.component.ts
+++ src/app/components/toggle-mode/toggle-mode.component.ts
@@ -1,14 +1,19 @@
-import { Component, Renderer2, ElementRef } from '@angular/core';
+import {Component, Renderer2, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toggle-mode',
   templateUrl: './toggle-mode.component.html',
   styleUrls: ['./toggle-mode.component.scss']
 })
-export class ToggleModeComponent {
+export class ToggleModeComponent implements OnInit{
   constructor(private renderer: Renderer2, private el: ElementRef) {
   }
-  mode: boolean = false;
+  mode: boolean =false;
+  ngOnInit() {
+    const theamMode = window.localStorage.getItem('theme');
+    if(theamMode == 'dark'){this.mode = true}else{this.mode = false}
+  }
+
   modeEvent(){
 
     const htmlElement = this.el.nativeElement.ownerDocument.body.parentElement;
@@ -16,11 +21,14 @@ export class ToggleModeComponent {
 
     if (!this.mode) {
       htmlElement.classList.add(className);
+      localStorage.setItem('theme','dark')
+      this.mode = false
     } else {
       htmlElement.classList.remove(className);
+      localStorage.setItem('theme','light')
+      this.mode = true
     }
     this.mode = !this.mode;
-     // Викликаємо modeEvent() після додавання класу
   }
 
 }
